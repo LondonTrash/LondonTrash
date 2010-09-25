7e5b3309c709db8d61a1644ac63f537e4059d813
Stephen Henderson <me@steveh.ca>
2010-09-25 14:10:10 -0400

    Admin controller/view framework started

---
+++ controllers/admins_controller.php
@@ -0,0 +1,14 @@
+<?php
+class AdminsController extends AppController {
+	var $name = 'Admins';
+	var $helpers = array('Session');
+	
+	function login() {
+		
+	}
+	
+	function logout() {
+	
+	}
+}
+?>
