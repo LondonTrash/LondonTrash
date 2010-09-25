c3abc069dacef481c3d0eca92dc7bdc96fa02760
Scott Reeves <cottser@mailworks.org>
2010-09-24 21:04:46 -0400

    Updating salt and cipherSeed values.

--- config/core.php
+++ config/core.php
@@ -200,12 +200,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '0AJsxtopgScBYwfXjElKPXajp9r35tYjRj');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '306322721462952087111466473619');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
