0b437cb7003d48daeb9c1f609704d18431e13595
Gavin Blair <gavin.blair@rtraction.com>
2010-09-25 19:05:16 -0400

    database fix

---
+++ config/migrations/006_daaaaang.php
@@ -0,0 +1,64 @@
+<?php
+class M4c9e7fa1581441bbad5564213201ca43 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'contents' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'body' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'created' => array('type' => 'date', 'null' => false, 'default' => NULL),
+					'modified' => array('type' => 'date', 'null' => false, 'default' => NULL),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'contents'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
--- config/migrations/map.php
+++ config/migrations/map.php
@@ -8,7 +8,9 @@ $map = array(
 		'003_add_ical_url_to_zone' => 'M4c9e4414024c48be864b03b23201ca43'),
 	4 => array(
 		'004_add_zone_table_data' => 'M4c9e6729dbc04c19bc68093b3201ca43'),
-        5 => array(
+	5 => array(
 		'005_adding_content_table' => 'M4c9e675fdd384307957b52413201ca43'),
+	6 => array(
+		'006_daaaaang' => 'M4c9e7fa1581441bbad5564213201ca43'),
 );
 ?>
