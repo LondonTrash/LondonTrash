4c1f0ac1ffd65d134c7af348476d46b967bed701
SeanJA <sean.sandy@gmail.com>
2010-09-25 14:51:50 -0300

    =added a method to retrieve the zone from the database (it will return false all the time as there is no table/schema at the moment in which to store the zone-names), if this fails, it will look up the zone via the webservice.

--- models/zone.php
+++ models/zone.php
@@ -10,14 +10,33 @@ class Zone extends AppModel {
 
         public function get_zone($address)
         {
-            //find the zone
+			//if we haven't got the zone locally, get it from the openhalton database
+			if(!$zone_name = $this->get_zone_local($address)){
+				$zone_name = $this->get_zone_openhalton($address);
+			}
+			return $zone_name;
+        }
+
+		private function get_zone_openhalton($address){
+			//find the zone
             $contents = file_get_contents("http://openhalton.ca/londontrash/LondonTrash.svc/GetZone?address=" . urlencode($address) . "&mapprovider=bing");
             $contents = json_decode($contents);
-
-            $zone_name = $contents->d->ZoneText;
+			var_dump($contents->d->ZoneText);
+			$zone_name = false;
+			if(!empty($contents->d->ZoneText)){
+				$zone_name = $contents->d->ZoneText;
+			}
+			//TODO: save zone so we don't have to make a call to a service that may or may not be up all the time
 
             return $zone_name;
-        }
+		}
+
+		private function get_zone_local($address){
+			//parse out postal code
+			//get the zone based on the postal code
+			//if zones can change: make sure that the zone retrieval date is not greater than x
+			return false;
+		}
 
         public function get_schedule($zone)
         {
