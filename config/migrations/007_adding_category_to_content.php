558705e20c833e874cfab898390068d48eab7e7b
Scott Reeves <cottser@mailworks.org>
2010-09-25 21:00:16 -0400

    Re-doing incorrect migration

---
+++ config/migrations/007_adding_category_to_content.php
@@ -0,0 +1,55 @@
+<?php
+class M4c9e9aed9ac04366899a29143201ca43 extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'contents' => array(
+					'category' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'contents' => array('category',),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
