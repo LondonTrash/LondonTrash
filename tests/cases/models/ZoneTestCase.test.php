4ab05f5fb0d34fcad7513ad0af972b08943f5521
amcgowanca <me@amcgowan.ca>
2010-09-26 14:17:42 -0400

    Initial commit

--- tests/cases/models/ZoneTestCase.test.php
+++ tests/cases/models/ZoneTestCase.test.php
@@ -11,12 +11,15 @@ class ZoneTestCase extends CakeTestCase {
         $this->assertEqual("F", $model->get_zone("N6C 3Z4")); // pass
         
         $this->assertEqual("B", $model->get_zone("30 Silverbrook Drive London Ontario")); // pass
+        $this->assertEqual("B", $model->get_zone("N5X 3B3")); // pass
         
-        $this->assertEqual("B", $model->get_zone("N5X 3B3"));
+        
     }
     
     public function testGetSchedule() {
         $model = new Zone();
+        
+        
         
         // $model->get_scheduale("F");
         // array( Array ( [type] => pickup [start_date] => 1286164800 [end_date] => 1286251200 [description] => ) );
