65b15137179d54a132ac6a115dc05409120e9f4d
Noah <noah13013@gmail.com>
2010-09-26 12:44:23 -0400

    If a zone is not returned, try to append the city and province to the search

--- controllers/searches_controller.php
+++ controllers/searches_controller.php
@@ -7,20 +7,32 @@ class SearchesController extends Controller {
     {
         if($this->data)
         {
-            $zone = $this->Zone->get_zone($this->data['Search']['Address']);
+			$searchAddress = $this->data['Search']['Address'];
+            $zone = $this->Zone->get_zone($searchAddress);
+            
+            //if zone is empty, try to append city
+            $cities = array('London', 'Byron', 'Lambeth', 'Hyde Park');
+            foreach($cities as $city)
+            {
+				if (empty($zone))
+				{
+					$searchAddress = $searchAddress .= ', ' . $city . ', ON';
+					$zone = $this->Zone->get_zone($searchAddress);
+				} else {
+					continue;
+				}
+			}
+            
             if(!empty($zone))
             {
                 $this->redirect(array("controller"=>"zones", "action"=>"view", $zone));
             }
             else
             {
-                $this->Session->setFlash("You FAIL!!!!");
+                $this->Session->setFlash("Your address was not found. Please verify that you have typed it correctly and search again.");
             }
         }
-        
     }
-
-
 };
 
 ?>
