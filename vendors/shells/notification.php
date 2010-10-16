f7de48666ba295f5937a03d74ade882d0fab9ca2
Scott Reeves <cottser@mailworks.org>
2010-10-16 00:27:47 -0400

    Regular pickups only, setting standard offset for notifications

--- vendors/shells/notification.php
+++ vendors/shells/notification.php
@@ -2,15 +2,18 @@
 class NotificationShell extends Shell {
 
 	var $uses = array('Subscriber', 'Zone');
-
+	
 	function main() {
-		//send out reminder emails
-		$this->out("This is your reminder!");
-
+		/**
+		 * How far before we should send the notification
+		 * This value is in seconds.
+		 * 
+		 * Since we're using all day events in gCal, 12am - 5h = 7pm the night before
+		 */
+		$notificationOffset = 60 * 60 * 5;
+		
 		//for each zone, get the next pickup times
 		$zones = $this->Zone->find('list', array('fields' => array('Zone.title', 'Zone.id')));
-		
-		#var_dump($zones);
 		
 		foreach ($zones as $zone_title => $zone_id) {
 			//get the schedule
@@ -18,18 +21,28 @@ class NotificationShell extends Shell {
 
 			//for each user in the zone
 			$subscribers = $this->Subscriber->find('all', array('conditions' => array('Subscriber.zone_id' => $zone_id)));
-
+			
 			foreach ($subscribers as $subscriber) {
 				foreach ($subscriber['Notification'] as $notification) {
 					foreach ($schedule as $pickup) {
-						$notification_time = $pickup['start_date'] - $notification['delay'] - strtotime('20 minutes');
 						
-						if (time() >= $notification_time && ($notification['last_sent'] == null || $notification['last_sent'] > $notification_time)) {
-							$this->Subscriber->Notification->id = $notification['id'];
-							$this->Subscriber->Notification->saveField('last_sent', time());
+						// when to notify
+						$notification_time = $pickup['start_date'] - $notificationOffset;
+						
+						// only look at events in the future.
+						// also, let's just worry about regular pickups for now.
+						if (time() <= $notification_time && $pickup['type'] == 'pickup') {
+							
+							// check to see if we've already sent a notification
+							if ($notification['last_sent'] == null || $notification['last_sent'] < $notification_time) {
+								$this->Subscriber->Notification->id = $notification['id'];
+								$this->Subscriber->Notification->saveField('last_sent', time());
 
-							echo "Sending notification to" . $subscriber["Subscriber"]["contact"] . " about a pickup on " . $pickup['start_date'];
-							var_dump($subscriber["Subscriber"]["contact"]);
+								$this->out("\n\nSending notification to:\n" . $subscriber["Subscriber"]["contact"] .
+									" in Zone " . $zone_title . " about a pickup on " . date('F j Y', $pickup['start_date']) .
+									"\nNotification will be sent at " . date('r', $notification_time));
+							}
+						
 						}
 					}
 					#	 var_dump($notification);
