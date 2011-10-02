7f9b8881714419f0de401a45222ac790dcd9d5f0
Scott Reeves <cottser@mailworks.org>
2011-10-02 00:46:59 -0400

    Adding handling for recurring events

--- models/schedule.php
+++ models/schedule.php
@@ -11,16 +11,35 @@ class Schedule extends AppModel {
 		$ical = new SG_iCal($url);
 		$schedule = array();
 		$events = $ical->getEvents();
-		
-		if( is_array($events) ) {
-			foreach( $events as $event ) {
-				$schedule[] = array(
-					'type' => $event->getSummary(),
-					'start_date' => $event->getStart(),
-					'end_date' => $event->getEnd(),
-					'description' => $event->getDescription()
-				);
-			}
+
+		if (!is_array($events)) { return array(); }
+
+		foreach($events as $event) {
+			$calEvent = array(
+				'type' => $event->getSummary(),
+				'start_date' => $event->getStart(),
+				'end_date' => $event->getEnd()-1,
+				'description' => $event->getDescription()
+			);
+
+			if (isset($event->recurrence)) {
+				$count = 0;
+				$start = $event->getStart();
+				$freq = $event->getFrequency();
+				if ($freq->firstOccurrence() == $start) {
+					$schedule[] = $calEvent;
+				}
+				while (($next = $freq->nextOccurrence($start)) > 0 ) {
+					if (!$next or $count >= 1000) break;
+					$count++;
+					$start = $next;
+					$calEvent["start_date"] = $start;
+					$calEvent["end_date"] = $start + $event->getDuration()-1;
+
+					$schedule[] = $calEvent;
+				}
+			} 
+			$schedule[] = $calEvent;
 		}
 		
 		return $schedule;
